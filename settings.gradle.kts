53cea07711923779a7fa3d8179dca41f10609fb1
artistso <Soquarky@artistso.com>
2026-06-06 11:58:05 -0700

    Add Android and Kotlin plugin versions
    Added Android and Kotlin plugin versions to the plugins DSL.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,14 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        // Add the Android Gradle Plugin and Kotlin Android plugin versions here so the plugins DSL can resolve them.
+        id("com.android.application") version "8.1.2" apply false
+        id("com.android.library")     version "8.1.2" apply false
+        id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
